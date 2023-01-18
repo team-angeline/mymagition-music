7af9a2df5e05b252f047e418cf25bbaebcfb661f
recoma <seokbong60@gmail.com>
2023-01-19 00:19:40 +0900

    reset: fastapi work failed. so will be changed to flask

--- loader/router.py
+++
@@ -1,5 +0,0 @@
-from apps.pcfl import router as pcfl_router
-
-API_ROUTERS = [
-    pcfl_router,
-]
--- main.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-
-from loader.boot import Config, init_app
-
-config: Config = Config()
-app, conf = init_app(config)
-
-if __name__ == '__main__':
-    uvicorn.run('main:app', **conf, host='0.0.0.0')
